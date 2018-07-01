4b96a0e38ba297d187b1d4c2e15420fd2ffe24c0
Henry Cuffy <hcuffy_5@hotmail.com>
2018-07-01 18:08:02 +0200

    Create new issue.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const issueController = require('../controllers/issues');
 
+router.post('/new', issueController.createIssue);
 router.get('/', (req, res) => {
 	res.render('index');
 });
