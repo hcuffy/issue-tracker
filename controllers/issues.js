8f55f84bb8b89162dda70b38a147676b1386a8b2
Henry Cuffy <hcuffy_5@hotmail.com>
2018-07-04 17:31:08 +0200

    Fixing route

--- controllers/issues.js
+++ controllers/issues.js
@@ -2,7 +2,8 @@ const Issue = require('../models/issue');
 
 exports.createIssue = (req, res, next) => {
 	const { title, description, creator, assignee, status } = req.body;
-
+	console.log(req);
+	res.send(req.originalUrl);
 	const newIssue = new Issue({
 		title,
 		description,
